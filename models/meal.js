de773670d4e4628c67fb3a2264342cb4c5bf4ac5
randy-darsh <rspringer47@gmail.com>
2018-02-07 19:06:39 -0700

    fixing find meals and made explicit returns on the other functions

--- models/meal.js
+++ models/meal.js
@@ -9,21 +9,21 @@ var Meal = {
   }),
 
   find: function(id) {
-    database.raw('SELECT * FROM meals where id = ?', [id])
+    return database.raw('SELECT * FROM(foods INNER JOIN mealfoods ON foods.id = mealfoods.meal_id) AS t WHERE t.meal_id = 1')
     .then(function (foods) {
      return foods.rows
     })
   },
 
   new: function(meal_id, food_id) {
-    databse.raw('INSERT INTO mealfoods (meal_id, food_id) VALUES (?,?)', [meal_id, food_id])
+    return database.raw('INSERT INTO mealfoods (meal_id, food_id) VALUES (?,?)', [meal_id, food_id])
     .then(function (food) {
       return food.rows
     })
   },
 
   destroy: function(meal_id, food_id) {
-    database.raw('DELETE FROM mealsfoods WHERE meal_id ? AND food_id = ?', [meal_id, food_id])
+    return database.raw('DELETE FROM mealsfoods WHERE meal_id ? AND food_id = ?', [meal_id, food_id])
     .then(function (food) {
       return food
     })
