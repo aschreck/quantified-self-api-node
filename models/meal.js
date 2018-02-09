1da287975268c85008bf5b1e383cbcf42191d8b9
Aaron Schreck <schreck.aaron@gmail.com>
2018-02-08 23:03:28 -0700

    wip

--- models/meal.js
+++ models/meal.js
@@ -5,8 +5,10 @@ const database = require('knex')(configuration)
 var Meal = {
 
   all: function () {
-    return database.raw('SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id GROUP BY m.id')
+    return database.raw('SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN mealfoods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id GROUP BY m.id')
     .then(function(mealfoods){
+      pry = require('pryjs')
+      eval(pry.it)
       return mealfoods.rows
     })
   },
