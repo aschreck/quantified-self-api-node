c3005ffa1aaa968b66710d2feb3cf55b99dea293
Aaron Schreck <schreck.aaron@gmail.com>
2018-02-08 19:47:56 -0700

    spiking the spike

--- models/meal.js
+++ models/meal.js
@@ -2,11 +2,26 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+//problem: we need to build the json response so that it also has the meals associated with it.
 var Meal = {
-  all: database.raw('SELECT * FROM meals')
-  .then(function (meals) {
-    return meals.rows
-  }),
+
+  all: function() {
+    database.raw('SELECT * FROM meals')
+    .then(function(meals) {
+    return Promise.all(
+      meals.rows.map(function(meal){
+        return database.raw('SELECT * FROM foods WHERE id=?', [meal.id])
+        .then(function(foods) {
+          return mealWithFoods = {id: meal.id, name: meal.name, foods: foods.rows}
+        })
+      })
+    )
+    .then(function(allmeals) {
+      return allmeals
+    })
+  })
+},
+  //construct an array of promises. For each item in row, create a promise
 
   find: function (id) {
     return database.raw('SELECT * FROM(foods INNER JOIN mealfoods ON foods.id = mealfoods.meal_id) AS t WHERE t.meal_id = 1')
